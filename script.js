acdd49515b8173acc9e1d4f2fb4d20544c70860a
maryamarj <maryam.arjmandi12@gmail.com>
2023-07-06 13:24:32 +0200

    Ground Floor Finished

--- script.js
+++ script.js
@@ -189,6 +189,14 @@ viewer = pannellum.viewer('panorama', {
                     "type": "scene",
                     "text": "Go to Space2",
                     "sceneId": "Space2", 
+                },
+                {
+                    "cssClass": "custom-hotspot-component",
+                    "pitch": -25,
+                    "yaw": -155,
+                    "type": "scene",
+                    "text": "Go to Space8",
+                    "sceneId": "Space8",
                 }
             ]
         },
@@ -373,6 +381,182 @@ viewer = pannellum.viewer('panorama', {
                     "type": "scene",
                     "text": "Go back to Space6",
                     "sceneId": "Space6",  
+                },
+                {
+                    "cssClass": "custom-hotspot-component",
+                    "pitch": -20,
+                    "yaw": 170,
+                    "type": "scene",
+                    "text": "Go back to Space1",
+                    "sceneId": "Space1",
+                }
+            ]
+        },
+        "Space8": {
+            "title": "Space8",
+            "cssClass": "title-background-color",
+            "hfov": 300,
+            "pitch": -5,
+            "yaw": -80, //(da dx a sx)
+            "type": "equirectangular",
+            // "panorama": api_endpoint + "/" + room_name ,
+            "panorama": api_endpoint + "/Space8", 
+            "hotSpots": [
+                {
+                    "cssClass": "custom-hotspot-component",
+                    "pitch": -25,
+                    "yaw": 33,
+                    "type": "scene",
+                    "text": "Go back to Space1",
+                    "sceneId": "Space1",  
+                },
+                {
+                    "cssClass": "custom-hotspot-component",
+                    "pitch": -25,
+                    "yaw": -160,
+                    "type": "scene",
+                    "text": "Go to Space9",
+                    "sceneId": "Space9",
+                }
+            ] 
+        },
+        "Space9": {
+            "title": "Space9",
+            "cssClass": "title-background-color",
+            "hfov": 300,
+            "pitch": -5,
+            "yaw": -80, //(da dx a sx)
+            "type": "equirectangular",
+            // "panorama": api_endpoint + "/" + room_name ,
+            "panorama": api_endpoint + "/Space9", 
+            "hotSpots": [
+                {
+                    "cssClass": "custom-hotspot-component",
+                    "pitch": -25,
+                    "yaw": -160,
+                    "type": "scene",
+                    "text": "Go back to Space8",
+                    "sceneId": "Space8",
+                },
+                {
+                    "cssClass": "custom-hotspot-component",
+                    "pitch": -20,
+                    "yaw": -15,
+                    "type": "scene",
+                    "text": "Go to Space10",
+                    "sceneId": "Space10",
+                }
+            ]
+        },
+        "Space10": {
+            "title": "Space10",
+            "cssClass": "title-background-color",
+            "hfov": 300,
+            "pitch": -5,
+            "yaw": -80, //(da dx a sx)
+            "type": "equirectangular",
+            // "panorama": api_endpoint + "/" + room_name ,
+            "panorama": api_endpoint + "/Space10" ,
+            "hotSpots": [
+                {
+                    "cssClass": "custom-hotspot-component",
+                    "pitch": -20,
+                    "yaw": 160,
+                    "type": "scene",
+                    "text": "Go back to Space9",
+                    "sceneId": "Space9",
+                },
+                {
+                    "cssClass": "custom-hotspot-component",
+                    "pitch": -20,
+                    "yaw": -33,
+                    "type": "scene",
+                    "text": "Go to Space11",
+                    "sceneId": "Space11",
+                }
+            ]
+        },
+        "Space11": {
+            "title": "Space11",
+            "cssClass": "title-background-color",
+            "hfov": 300,
+            "pitch": -5,
+            "yaw": -80, //(da dx a sx)
+            "type": "equirectangular",
+            // "panorama": api_endpoint + "/" + room_name ,
+            "panorama": api_endpoint + "/Space11" ,
+            "hotSpots": [
+                {
+                    "cssClass": "custom-hotspot-component",
+                    "pitch": -20,
+                    "yaw": 160,
+                    "type": "scene",
+                    "text": "Go back to Space10",
+                    "sceneId": "Space10",  
+                },
+                {
+                    "cssClass": "custom-hotspot-component",
+                    "pitch": -20,
+                    "yaw": 10,
+                    "type": "scene",
+                    "text": "Go to Space13",
+                    "sceneId": "Space13", 
+                },
+                {
+                    "cssClass": "custom-hotspot-component",
+                    "pitch": -15,
+                    "yaw": -20,
+                    "type": "scene",
+                    "text": "Go to Space12",
+                    "sceneId": "Space12", 
+                }
+            ]
+        },
+        "Space13": {
+            "title": "Space13",
+            "cssClass": "title-background-color",
+            "hfov": 300,
+            "pitch": -5,
+            "yaw": -80, //(da dx a sx)
+            "type": "equirectangular",
+            // "panorama": api_endpoint + "/" + room_name ,
+            "panorama": api_endpoint + "/Space13" ,
+            "hotSpots": [
+                {
+                    "cssClass": "custom-hotspot-component",
+                    "pitch": -28,
+                    "yaw": -80,
+                    "type": "scene",
+                    "text": "Go back to Space11",
+                    "sceneId": "Space11",
+                },
+                {
+                    "cssClass": "custom-hotspot-component",
+                    "pitch": -23,
+                    "yaw": 0,
+                    "type": "scene",
+                    "text": "Go to Space12",
+                    "sceneId": "Space12", 
+                }
+            ]
+        },
+        "Space12": {
+            "title": "Space12",
+            "cssClass": "title-background-color",
+            "hfov": 300,
+            "pitch": -5,
+            "yaw": -80, //(da dx a sx)
+            "type": "equirectangular",
+            // "panorama": api_endpoint + "/" + room_name ,
+            "panorama": api_endpoint + "/Space12",
+            "hotSpots": [
+                {
+                    "cssClass": "custom-hotspot-component",
+                    "pitch": -10,
+                    "yaw": 155,
+                    "type": "scene",
+                    "text": "Go back to Space11",
+                    "sceneId": "Space11",
                 }
             ]
         }
